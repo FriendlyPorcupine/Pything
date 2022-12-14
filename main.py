7dd767695a18e5e99ca433187480bfbe900edfc5
ZoeMRuf <92019231+ZoeMRuf@users.noreply.github.com>
2022-12-14 21:32:14 +0100

    move_tile function and test sequence in main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-# This is the main python file
+# import the puzzle class
+import puzzel as p
 
 if __name__ == '__main__':
-    print('Hello, This is the main Function!')
+    puzzle = p.init_puzzle()
+    p.print_puzzle(puzzle)
+    if p.puzzle_inorder(puzzle) == 0:
+        print('Make a move: ')
+        direction = 'up'
+        p.move_tile(puzzle, direction)
+    else:
+        print('The Puzzle is solved')
--- puzzel.py
+++ puzzel.py
@@ -28,15 +28,15 @@ def print_puzzle(puzzleToPrint):
 
 
 def find_empty(puzzle):
-    row = 0
-    col = 0
+    empty_row = 0
+    empty_col = 0
 
     for i in range(3):
         for j in range(3):
             if puzzle[i][j] == 0:
-                row = i
-                col = j
-    return row, col
+                empty_row = i
+                empty_col = j
+    return empty_row, empty_col
 
 
 def puzzle_inorder(puzzle):
@@ -47,7 +47,7 @@ def puzzle_inorder(puzzle):
     # make boolean value for return statement & set to true (1) as a start value
     inOrder = 1
 
-    print(puzzle, '==', puzzleInOrder)  # only for now to check the function
+    # print(puzzle, '==', puzzleInOrder)  # only for now to check the function
 
     for i in puzzleInOrder:
         if puzzle[i] != puzzleInOrder[i]:
@@ -56,26 +56,42 @@ def puzzle_inorder(puzzle):
     return bool(inOrder == 1)
 
 
-"""
 # Define a function to move a tile in the puzzle
 def move_tile(puzzle, direction):
     # Get the position of the empty tile
-    for i in range(3):
-        for j in range(3):
-            if puzzle[i][j] == 0:
-                empty_row = i
-                empty_col = j
-                break
+    empty_row = find_empty(puzzle)[0]
+    empty_col = find_empty(puzzle)[1]
 
-    # Move the empty tile in the specified direction
+    print('empty_tile: ', empty_row, empty_col)
+
+    # Move the empty tile in the specified direction up & down change the row, left & right change the col
     if direction == "up" and empty_row > 0:
         puzzle[empty_row][empty_col] = puzzle[empty_row - 1][empty_col]
-        puzzle[empty_row - 1][empty_col]
-"""
+        puzzle[empty_row - 1][empty_col] = 0
+        print_puzzle(puzzle)
+
+    elif direction == "down" and empty_row < 2:
+        puzzle[empty_row][empty_col] = puzzle[empty_row + 1][empty_col]
+        puzzle[empty_row + 1][empty_col] = 0
+        print_puzzle(puzzle)
+
+    elif direction == "left" and empty_col > 0:
+        puzzle[empty_row][empty_col] = puzzle[empty_row][empty_col - 1]
+        puzzle[empty_row][empty_col - 1] = 0
+        print_puzzle(puzzle)
+
+    elif direction == "right" and empty_col < 2:
+        puzzle[empty_row][empty_col] = puzzle[empty_row][empty_col + 1]
+        puzzle[empty_row][empty_col + 1] = 0
+        print_puzzle(puzzle)
+    else:
+        print('move not possible')
+
 
 # To test the functions
 if __name__ == '__main__':
     p = init_puzzle()
     print_puzzle(p)
-    print(puzzle_inorder(p))
-    print(find_empty(p))
+    #print(puzzle_inorder(p))
+    #print(find_empty(p))
+    #move_tile(p, "right")
