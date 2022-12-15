ac68af785811bf441da0bfa62cd4106df8b4706d
ZoeMRuf <92019231+ZoeMRuf@users.noreply.github.com>
2022-12-15 12:01:43 +0100

    some comments

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 import puzzel as p
 
 if __name__ == '__main__':
-    puzzle = p.init_puzzle()
+    puzzle = p.init_puzzle()  # Create puzzle to play & print it to console
     p.print_puzzle(puzzle)
-    if p.puzzle_inorder(puzzle) == 0:
-        print('Make a move: ')
-        direction = 'up'
+
+    while p.puzzle_inorder(puzzle) != 1:  # Loop until the puzzle is solved
+        direction = input('Make a move (up, down, left, right): ')  # Scan the move of the player
         p.move_tile(puzzle, direction)
-    else:
-        print('The Puzzle is solved')
+
+    print('The puzzle is solved')
+    p.print_puzzle(puzzle)
+
--- puzzel.py
+++ puzzel.py
@@ -4,53 +4,49 @@ import numpy as np
 
 # Define a function to initialize the puzzle
 def init_puzzle():
-    # Create a array to represent the puzzle
-    puzzle = np.arange(9)
-    # Shuffle the puzzle to create a random starting configuration
-    np.random.shuffle(puzzle)
-    # Shape the array into a 2D array after it was shuffled to to assure rows and cols get mixed
-    shuffledPuzzle = puzzle.reshape((3, 3))
+    puzzle = np.arange(9)  # Create a array to represent the puzzle
+    np.random.shuffle(puzzle)   # Shuffle the puzzle to create a random starting configuration
 
+    # Shape the array into a 2D array AFTER it was shuffled to assure rows and cols get mixed
+    shuffledPuzzle = puzzle.reshape((3, 3))
     return shuffledPuzzle
 
 
-# To print the puzzle to the console
+# Define a function to print the puzzle to the console
 def print_puzzle(puzzleToPrint):
-    for i in range(3):
+    for i in range(3):  # iterate ofer the rows of the puzzle
         print(' +---+---+---+')
-        for j in range(3):
-            if puzzleToPrint[i][j] == 0:
+        for j in range(3):  # iterate ofer the cols of the puzzle
+            if puzzleToPrint[i][j] == 0:  # print the value of the tile
                 print(' |', ' ', end='')
             else:
-                print(' |', puzzleToPrint[i][j], end='')
+                print(' |', puzzleToPrint[i][j], end='')  # print nothing if value is zero (0) -> empty tile
         print(' |')
     print(' +---+---+---+')
 
 
+# Define a function to find the position of the empty tile
 def find_empty(puzzle):
     empty_row = 0
     empty_col = 0
 
-    for i in range(3):
-        for j in range(3):
-            if puzzle[i][j] == 0:
+    for i in range(3):  # iterate ofer the rows of the puzzle
+        for j in range(3):  # iterate ofer the cols of the puzzle
+            if puzzle[i][j] == 0:  # find tile with value zero (0) & remember position in the 2D array
                 empty_row = i
                 empty_col = j
     return empty_row, empty_col
 
 
+# Define a function to check if the puzzle is solved
 def puzzle_inorder(puzzle):
-    # Make array to compare to
-    puzzleInOrder = np.arange(9)
-    # reshape puzzle to array
-    puzzle = puzzle.reshape(9)
-    # make boolean value for return statement & set to true (1) as a start value
-    inOrder = 1
+    puzzleInOrder = np.arange(9)  # Make an array with values in the correct order to compare to
+    puzzle = puzzle.reshape(9)  # Reshape 2D array puzzle to simple array
 
-    # print(puzzle, '==', puzzleInOrder)  # only for now to check the function
+    inOrder = 1  # make boolean value for return statement & set to true (1) as a start value
 
-    for i in puzzleInOrder:
-        if puzzle[i] != puzzleInOrder[i]:
+    for i in puzzleInOrder:  # iterate ofer the hole array
+        if puzzle[i] != puzzleInOrder[i]:  # check if the values of puzzle don't match the array with correct order
             inOrder = 0
             break
     return bool(inOrder == 1)
@@ -58,11 +54,7 @@ def puzzle_inorder(puzzle):
 
 # Define a function to move a tile in the puzzle
 def move_tile(puzzle, direction):
-    # Get the position of the empty tile
-    empty_row = find_empty(puzzle)[0]
-    empty_col = find_empty(puzzle)[1]
-
-    print('empty_tile: ', empty_row, empty_col)
+    empty_row, empty_col = find_empty(puzzle)[0], find_empty(puzzle)[1]  # Get the position of the empty tile
 
     # Move the empty tile in the specified direction up & down change the row, left & right change the col
     if direction == "up" and empty_row > 0:
@@ -91,7 +83,7 @@ def move_tile(puzzle, direction):
 # To test the functions
 if __name__ == '__main__':
     p = init_puzzle()
-    print_puzzle(p)
-    #print(puzzle_inorder(p))
-    #print(find_empty(p))
-    #move_tile(p, "right")
+    # print_puzzle(p)
+    # print(puzzle_inorder(p))
+    # print(find_empty(p))
+    # move_tile(p, "right")
