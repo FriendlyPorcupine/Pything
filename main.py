d7a9f662b67b0b89627be065e95515a836e046b4
ZoeMRuf <92019231+ZoeMRuf@users.noreply.github.com>
2022-12-20 18:34:41 +0100

    puzzle_solvable function added in puzzle.py

--- main.py
+++ main.py
@@ -19,11 +19,14 @@ def average(arrayToCalculate):
     allMovesTogether = 0
     for i in arrayToCalculate:
         allMovesTogether += i  # add the moves needed to solve the puzzle
-    averageMovesNeeded = allMovesTogether / arrayToCalculate + 1
+    averageMovesNeeded = allMovesTogether / len(arrayToCalculate)
     return averageMovesNeeded
 
 
 def play_puzzle_game():
+    """
+    To play the 8-Puzzle as a User on the Console
+    """
     puzzle = p.init_puzzle()  # Create puzzle to play & print it to console
     p.print_puzzle(puzzle)
 
--- puzzle.py
+++ puzzle.py
@@ -2,8 +2,12 @@
 import numpy as np
 
 
-# Define a function to initialize the puzzle
 def init_puzzle():
+    """
+    Define a function to initialize the puzzle
+
+    :return: 2D Array with random puzzle
+    """
     puzzle = np.arange(9)  # Create a array to represent the puzzle
     np.random.shuffle(puzzle)  # Shuffle the puzzle to create a random starting configuration
 
@@ -11,8 +15,12 @@ def init_puzzle():
     return puzzle.reshape((3, 3))
 
 
-# Define a function to print the puzzle to the console
 def print_puzzle(puzzleToPrint):
+    """
+    Define a function to print the puzzle to the console
+
+    :param puzzleToPrint: 2D Array
+    """
     for i in range(3):  # iterate ofer the rows of the puzzle
         print(' +---+---+---+')
         for j in range(3):  # iterate ofer the cols of the puzzle
@@ -24,16 +32,26 @@ def print_puzzle(puzzleToPrint):
     print(' +---+---+---+')
 
 
-# Define a function to find the position of the empty tile
 def find_empty(puzzle):
+    """
+    Define a function to find the position of the empty tile
+
+    :param puzzle: 2D Array
+    :return: index of empty tile
+    """
     for i in range(3):  # iterate ofer the rows of the puzzle
         for j in range(3):  # iterate ofer the cols of the puzzle
             if puzzle[i][j] == 0:  # find tile with value zero (0) & remember position in the 2D array
                 return i, j
 
 
-# Define a function to check if the puzzle is solved
 def puzzle_unordered(puzzle):
+    """
+    Define a function to check if the puzzle is solved
+
+    :param puzzle: 2D Array
+    :return: Boolean
+    """
     puzzleInOrder = np.arange(9)  # Make an array with values in the correct order to compare to
     puzzle = puzzle.reshape(9)  # Reshape 2D array puzzle to simple array
 
@@ -43,23 +61,26 @@ def puzzle_unordered(puzzle):
     return False
 
 
-# Define a function to move a tile in the puzzle
 def move_tile(puzzle, direction):
+    """
+    Define a function to move a tile in the puzzle
+
+    :param puzzle: 2D Array
+    :param direction: up, down, left, right
+    :return: Boolean
+    """
     empty_row, empty_col = find_empty(puzzle)  # Get the position of the empty tile
 
     # Move the empty tile in the specified direction up & down change the row, left & right change the col
     if direction == "up" and empty_row > 0:
         puzzle[empty_row][empty_col] = puzzle[empty_row - 1][empty_col]
         puzzle[empty_row - 1][empty_col] = 0
-
     elif direction == "down" and empty_row < 2:
         puzzle[empty_row][empty_col] = puzzle[empty_row + 1][empty_col]
         puzzle[empty_row + 1][empty_col] = 0
-
     elif direction == "left" and empty_col > 0:
         puzzle[empty_row][empty_col] = puzzle[empty_row][empty_col - 1]
         puzzle[empty_row][empty_col - 1] = 0
-
     elif direction == "right" and empty_col < 2:
         puzzle[empty_row][empty_col] = puzzle[empty_row][empty_col + 1]
         puzzle[empty_row][empty_col + 1] = 0
@@ -68,19 +89,25 @@ def move_tile(puzzle, direction):
     return True
 
 
+def puzzle_solvable(puzzle):
+    """
+    Define a function to find out if the puzzle is solvable
+
+    :param puzzle: 2D Array
+    :return: boolean
+    """
+    inverseCount = 0
+    puzzle = puzzle.reshape(9)
+    # print(puzzle)
+    for i in range(9):
+        for j in range(i + 1, 9):
+            if puzzle[i] > puzzle[j] and (puzzle[j] != 0 and puzzle[i] != 0):
+                inverseCount += 1
+                # print(puzzle[i], '>', puzzle[j])
+    return bool((inverseCount % 2) == 0)
+
 # maybe add function to count how many tiles are already at a correct position?
 # To test the functions
 
-"""
-if __name__ == '__main__':
-    p = init_puzzle()
-    print_puzzle(p)
-    print(puzzle_inorder(p))
-    print(find_empty(p))
-    move_tile(p, "right")
-"""
 
-
-
-
-
+# if __name__ == '__main__':
