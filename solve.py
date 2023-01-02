0067b26776eadf5fc22a5001e70a0c6612fedb5f
friendlyPorcupine <max.kell@tuta.io>
2023-01-02 16:05:41 +0100

    Julian A* unfinished

--- solve.py
+++ solve.py
@@ -1,6 +1,10 @@
 # import necessary classes
+from typing import Any
+
 import numpy as np
-import puzzle
+from queue import PriorityQueue
+from dataclasses import dataclass
+import puzzle as p
 
 directions = ['up', 'down', 'left', 'right']  # array with the four possible directions
 
@@ -18,15 +22,73 @@ def random_solve(unsolvedPuzzle):
     return countMoves
 
 
+class Node:
+    puzzle_state: Any
+    steps: int
+
+    def __init__(self, puzzle_state, steps):
+        self.puzzle_state = puzzle_state
+        self.steps = steps
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+
+        return self.puzzle_state == other.puzzle_state
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash(self.puzzle_state)
+
+
+def a_star_solve(unsolved_puzzle, h):
+    open_list = PriorityQueue()
+    closed_list = set()
+
+    open_list.put((0, Node(unsolved_puzzle, 0)))
+
+    while not open_list.empty():
+        current_node = open_list.get()
+
+        if not p.puzzle_unordered(current_node.puzzle_state):
+            return 0
+
+        closed_list.add(current_node)
+        expand(open_list, closed_list, h, current_node)
+
+    raise Exception("no solution found")
+
+
+def expand(open_list, closed_list, h, current_node):
+    moves = p.moves_possible(current_node.puzzle_state)
+
+    for successor_move in moves:
+        new_puzzle_state = current_node.puzzle_state.copy()
+        p.move_tile(new_puzzle_state, successor_move)
+        successor_node = Node(new_puzzle_state, current_node.steps + 1)
+
+        if successor_node in closed_list:
+            continue
+
+        tentative_g = current_node.steps + 1
+
+        if open_list.get() and (tentative_g >= successor_node.steps):
+            continue
+
+        open_list.put((tentative_g + h(successor_node.puzzle_state), successor_node))
+
+
 # To test the functions
-"""
 if __name__ == '__main__':
     puzzle = p.init_puzzle()  # init_puzzle function must not shuffle
     p.move_tile(puzzle, 'down')  # Make puzzle with just one wrong tile
 
-    movesToSolve = random_solve(puzzle)
+    # movesToSolve = random_solve(puzzle)
+    movesToSolve = a_star_solve(puzzle, p.hamming_heuristic)
 
     print(movesToSolve, 'moves were necessary to solve the puzzle')
     p.print_puzzle(puzzle)
-"""
+
 
